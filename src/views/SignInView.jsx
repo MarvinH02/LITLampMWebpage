67907b2a8d5a2a2619c6c13c29508f300b7cbd2a
vfredl <108457182+MrBregott@users.noreply.github.com>
2024-04-17 18:33:35 +0200

    Added back skeleton code for Model-View-Presenter architecture and a reactive model

---
+++ src/views/SignInView.jsx
@@ -0,0 +1,11 @@
+
+function SignInView (props) {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <button >Sign In</button>
+    </div>
+  )
+}
+
+export default SignInView;
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import vueJsxPlugin from '@vitejs/plugin-vue-jsx';
+import vuePlugin from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        vueJsxPlugin(),
+        vuePlugin(),
+    ],
+    server: {
+        port: 8080,
+    },
+    build: {
+        sourcemap: true,
+        minify: false
+    }
+});
