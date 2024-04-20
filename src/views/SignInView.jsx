bcc0b22266706a98c506f3d707e7b7f7a42fe591
vfredl <108457182+MrBregott@users.noreply.github.com>
2024-04-20 22:37:03 +0200

    Added account information when logged in

--- src/views/SignInView.jsx
+++ src/views/SignInView.jsx
@@ -9,16 +9,35 @@ function SignInView (props) {
   function testCounterPlusCB() {
     props.testCounterPlusCustomEventCB();
   }
+  if (props.user) {
+    console.log("User signed in");
+    console.log(props.user)
+    return (
+      <div>
+        <h1>Account</h1>
+        <img src={props.user.photoURL} alt="User Photo"/>
+        <p>{props.user.displayName}</p>
+        <p>{props.user.email}</p>
+        <button onClick={signInWithGoogleButtonPressedCB}>{props.user ? <p>Sign Out</p> : <p>Sign In</p>}</button>
+        <button onClick={goToHomePageACB}>Go to Home Page</button>
+        <button onClick={testCounterPlusCB}>Test Counter +1</button>
+        <p>Counter: {props.testCounter}</p>
+      </div>
+    )
+  }
+  else{
+    console.log("User not signed in");
+    return (
+      <div>
+        <h1>Sign In</h1>
+        <button onClick={signInWithGoogleButtonPressedCB}>{props.user ? <p>Sign Out</p> : <p>Sign In</p>}</button>
+        <button onClick={goToHomePageACB}>Go to Home Page</button>
+        <button onClick={testCounterPlusCB}>Test Counter +1</button>
+        <p>Counter: {props.testCounter}</p>
+      </div>
+    )
+  }
 
-  return (
-    <div>
-      <h1>Sign In</h1>
-      <button onClick={signInWithGoogleButtonPressedCB}>{props.user ? <p>Sign Out</p> : <p>Sign In</p>}</button>
-      <button onClick={goToHomePageACB}>Go to Home Page</button>
-      <button onClick={testCounterPlusCB}>Test Counter +1</button>
-      <p>Counter: {props.testCounter}</p>
-    </div>
-  )
 }
 
 export {SignInView}
