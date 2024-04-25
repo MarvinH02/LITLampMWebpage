58c2ed3269938bd058d4ac1e1c6a680b813fde65
vfredl <108457182+MrBregott@users.noreply.github.com>
2024-04-25 21:31:28 +0200

    removed unnecessary function from AboutUsView

--- src/presenters/AboutUsPresenter.jsx
+++ src/presenters/AboutUsPresenter.jsx
@@ -2,10 +2,6 @@ import {AboutUsView} from '../views/AboutUsView.jsx';
 
 export default function AboutUsPresenter(props) {
 
-  function changeBurgerState(){
-    props.model.setShowHamburger();
-  }
-
   return (
     <div>
       <AboutUsView 
