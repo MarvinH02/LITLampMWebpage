2b879c71cb91f2d26233b36f4fd13aa0635aea34
EroHex <william030428@gmail.com>
2024-05-02 01:23:54 +0200

    created images and game select view games
    the brain image is ai generated

--- src/views/GameSelectView.jsx
+++ src/views/GameSelectView.jsx
@@ -3,9 +3,62 @@ import '../style.css';
 function GameSelectView (props) {
 
     return (
-        <div className='center'>
-            GAME SELECT
+        <div className='border'>
+            <div className='gamebox'>
+                <div>
+                    <v-card 
+                    text="" 
+                    variant="elevated"
+                    hover
+                    link
+                    width= '50px'
+                    min-width='250px'
+                    height='50vh'
+                    image='src/images/tic.png'
+                    >
+                    <v-card-text style="padding-top:90%; text-align: center; font-size:x-large; font-family:fantasy">
+                        Tic-Tac-Toe
+                    </v-card-text>
+                    </v-card>
+                    
+                </div>
+                <div>
+                    <v-card 
+                    text="" 
+                    variant="elevated"
+                    hover
+                    link
+                    width= '50px'
+                    min-width='250px'
+                    height='50vh'
+                    image='src/images/snake.png'
+                    >
+                    <v-card-text style="padding-top:90%; text-align: center; font-size:x-large; font-family:fantasy">
+                        Snake
+                    </v-card-text>
+                    </v-card>
+                    
+                </div>
+                <div>
+                    <v-card 
+                    text="" 
+                    variant="elevated"
+                    hover
+                    link
+                    width= '50px'
+                    min-width='250px'
+                    height='50vh'
+                    image='src/images/brain.png'
+                    >
+                    <v-card-text style="padding-top:90%; text-align: center; font-size:x-large; font-family:fantasy">
+                        Memory
+                    </v-card-text>
+                    </v-card>
+                    
+                </div>
+            </div>
         </div>
+        
     )
     
   }
