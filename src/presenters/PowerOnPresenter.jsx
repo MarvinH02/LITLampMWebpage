ab863ba5ee80af6256f75a754c49b759ce7bd4df
Goooooor <135389613+Goooooor@users.noreply.github.com>
2024-05-02 16:59:07 +0200

    updated poweronpresenter

--- src/presenters/PowerOnPresenter.jsx
+++ src/presenters/PowerOnPresenter.jsx
@@ -1,12 +1,36 @@
-import {PowerOnView} from '../views/PowerOnView.jsx';
+import axios from 'axios';
+import { PowerOnView } from '../views/PowerOnView.jsx';
 
 export default function PowerOnPresenter(props) {
-    function togglePowerCustomEventHandlerACB(){
-      props.model.togglePower();
+    function togglePowerCustomEventHandlerACB() {
+        props.model.togglePower();
+        const apiUrl = props.model.poweredOn ? 
+            `http://172.20.10.9:3000/start-demo?brightness=${props.model.brightness}` : 
+            'http://172.20.10.9:3000/stop-demo';
+
+        axios.get(apiUrl)
+            .then(response => {
+                console.log('Server response:', response.data);
+            })
+            .catch(error => {
+                console.error('Error calling the server:', error);
+            });
     }
-    function brightnessInputCustomEventHandlerACB(number){
+
+    function brightnessInputCustomEventHandlerACB(number) {
         props.model.setBrightness(number);
+        if (props.model.poweredOn) { // Update brightness only if the matrix is powered on
+            const apiUrl = `http://172.20.10.9:3000/start-demo?brightness=${number}`;
+            axios.get(apiUrl)
+                .then(response => {
+                    console.log('Server response:', response.data);
+                })
+                .catch(error => {
+                    console.error('Error calling the server:', error);
+                });
+        }
     }
+
     return (
       <div>
         <PowerOnView 
@@ -17,4 +41,4 @@ export default function PowerOnPresenter(props) {
           brightness={props.model.brightness}/>
       </div>
     );
-  }
+}
