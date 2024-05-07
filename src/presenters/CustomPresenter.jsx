7d51eb391dbd7e205b47105af34e9f76e43dcdc7
vfredl <108457182+MrBregott@users.noreply.github.com>
2024-05-07 18:10:30 +0200

    Styled buttons on custom page and added code for connecting the buttons to raspberry pi

--- src/presenters/CustomPresenter.jsx
+++ src/presenters/CustomPresenter.jsx
@@ -6,12 +6,37 @@ export default function CustomPresenter(props) {
     function customCurrentTimeHandlerACB() {
         const currentTime = new Date().toLocaleTimeString(); //spara tiden
         console.log('Current Time:', currentTime); //console logga tiden.
+        const apiUrl =
+            `http://${props.model.activeDevice.ip}:3000/ //här ska det vara en endpoint som tar emot datumet`;
+
+        axios.get(apiUrl)
+            .then(response => {
+                console.log('Server response:', response.data);
+            })
+            .catch(error => {
+                console.error('Error calling the server:', error);
+            });
+    }
+    function customCurrentDateHandlerACB() {
+        const currentDate = new Date().toLocaleDateString(); //spara datum
+        console.log('Current Date:', currentDate); //console logga datumet.
+        const apiUrl =
+            `http://${props.model.activeDevice.ip}:3000/ //här ska det vara en endpoint som tar emot datumet`;
+
+        axios.get(apiUrl)
+            .then(response => {
+                console.log('Server response:', response.data);
+            })
+            .catch(error => {
+                console.error('Error calling the server:', error);
+            });
     }
 
     return (
         <div>
             <CustomView
                 customTimeCustomEvent={customCurrentTimeHandlerACB}
+                customDateCustomEvent={customCurrentTimeHandlerACB}
             />
         </div>
     );
--- src/views/CustomView.jsx
+++ src/views/CustomView.jsx
@@ -1,9 +1,25 @@
 import '../style.css';
 function CustomView(props) {
+    function clickedDisplayTimeCB() {
+        props.customTimeCustomEvent();
+    }
+    function clickedDisplayDateCB() {
+        props.customDateCustomEvent();
+    }
     return (
-        <div>
-            <button onClick={props.customTimeCustomEvent}>Display Current Time</button>
-            <p>Current Time: {props.customTime}</p>
+        <div class="centerCustom">
+            <v-btn variant="tonal" class="customButton" onClick={clickedDisplayTimeCB}
+            width="200"
+            height="100"
+            >
+                        <h2>Display Time</h2>
+            </v-btn>
+            <v-btn variant="tonal" class="customButton" onClick={clickedDisplayDateCB}
+            width="200"
+            height="100"
+            >
+                        <h2>Display Date</h2>
+            </v-btn>
         </div>
     );
 }
