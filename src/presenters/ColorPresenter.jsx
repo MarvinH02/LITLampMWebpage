04dad42be6d527ce3fd1e80a28e2fc973b3456ae
MrBregott <108457182+MrBregott@users.noreply.github.com>
2024-05-12 20:56:55 +0200

    Added hardware integration to color page

--- src/presenters/ColorPresenter.jsx
+++ src/presenters/ColorPresenter.jsx
@@ -1,14 +1,37 @@
 import { ColorView } from "../views/ColorView";
+import axios from 'axios';
 
 export default function ColorPresenter (props) {
     function updateColorCustonEventHandlerACB(color){
+        console.log("color before: "+props.model.color);
         props.model.setColor(color);
+        console.log("color after: "+props.model.color);
+        const apiUrl = 
+            `http://${props.model.activeDevice.ip}:3000/display-color?color=${props.model.color.replace("#", "")}`;
+
+        axios.get(apiUrl)
+            .then(response => {
+                console.log('Server response:', response.data);
+            })
+            .catch(error => {
+                console.error('Error calling the server:', error);
+            });
     }
+    const stopColorCustomEventHandlerACB = () => {
+        axios.get(`http://${props.model.activeDevice.ip}:3000/stop-color`)
+            .then(response => {
+                console.log('Color display stopped successfully:', response.data);
+            })
+            .catch(error => {
+                console.error('Failed to stop color display:', error);
+            });
+    };
     return (
         <div>
             <ColorView
             currentColor={props.model.color}
             updateColorCustonEventCB={updateColorCustonEventHandlerACB}
+            stopColorCustomEventCB={stopColorCustomEventHandlerACB}
             />
 
         </div>
--- src/views/ColorView.jsx
+++ src/views/ColorView.jsx
@@ -9,15 +9,20 @@ function ColorView (props){
         //console.log(color);
         props.updateColorCustonEventCB(color);
     }
+    function stopColorCB(){
+        props.stopColorCustomEventCB();
+    }
     return(
         <div class="colorView">
             <h2>Current Color: {props.currentColor}</h2>
-            <v-avatar color={props.currentColor ? props.currentColor : "#000000"} size="80"></v-avatar>
+            <v-avatar color={props.currentColor} size="80"></v-avatar>
             <div class="colorPicker">
                 <v-color-picker onUpdate:modelValue={colorChangedCB}></v-color-picker>
             </div>
             <div class="colorButton">
                 <v-btn size="x-large" variant="tonal" onClick={updateColorCB}>Update color</v-btn>
+                <v-btn size="x-large" variant="tonal" onClick={stopColorCB}>Stop displaying color</v-btn>
+
             </div>
         </div>
     ); 
